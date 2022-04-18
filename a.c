70f21d9ddec5ea8eee8ec9099421066453c42581
taksas <takuminakayama4869@gmail.com>
2022-04-18 20:23:31 +0900

    やっとこさ終わった構造化プログラミング

--- a.c
+++ a.c
@@ -1,26 +1,12 @@
 #include "stdio.h"
 
 int main(void) {
-    int h, w, c1 = 0, c2 = 0;
-    while(1) {
-        c1 = 0;
-        scanf("%d %d", &h, &w);
-        if ( h == 0 && w == 0) {
-            return 0;
+    int n ;
+    scanf("%d", &n);
+    for (int i = 1; i <= n; i++ ) {
+        if ( i % 3 == 0 || i % 10 == 3 || i >= 10 && (i / 10) % 10 == 3 ){
+            printf(" %d", i);
         }
-        for (int i = 0; i < h; i++) {
-            for (int j = 0; j < w; j++) {
-                if (c) {
-                    printf(".");
-                    c = 0;
-                } else {
-                    printf("#");
-                    c = 1;
-                }
-            }
-            printf("\n");
-        }
-        printf("\n");
     }
     printf("\n");
     return 0;
